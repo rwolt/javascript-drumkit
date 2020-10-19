5a65a70491ce7c2597fba707ab4b1727f399ddf1
Raymond Wolt <rjwolt@gmail.com>
2020-10-18 22:05:56 -0500

    Trigger sound with data-key

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-let pad = document.getElementsByClassName('btn');
+window.addEventListener('keydown', (e) => {
+        const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+        const key = document.querySelector(`div[data-key="${e.code}"]`);
+        if(!audio) return;
+        audio.currentTime = 0; //rewind to the start
+        audio.play();
+        key.classList.add('playing');
+    });
 
+    const keys = document.querySelectorAll('.btn');
+    
+    keys.forEach(key =>key.addEventListener(`transitionend`, removeTransition));
+
+    function removeTransition(e) {
+        if (e.propertyName !== `transform`)return;
+        this.classList.remove(`playing`);
+    }
